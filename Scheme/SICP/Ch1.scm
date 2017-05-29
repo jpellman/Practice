121f946445fe4801bc7bfe909352ce2f47c98687
John Pellman <pellman.john@gmail.com>
2017-05-28 22:25:42 -0400

    Finished exercise 1.3

--- Scheme/SICP/Ch1.scm
+++ Scheme/SICP/Ch1.scm
@@ -12,3 +12,25 @@
 ; 16
 ; 6
 ; 16
+; 
+; Exercise 1.2
+( /
+	( + 5 4 ( - 2
+			( - 3
+				( + 6
+					( / 1 3)
+				)
+			)
+		)
+	)
+	( * 3 
+		( - 6 2 )
+		( - 2 7 )
+	)
+)
+
+; Exercise 1.3
+( define (square a) ( * a a ) )
+( define (sum-of-squares a b) ( + (square a) (square b)))
+( define (returnlargest a b ) ( if ( > a b ) a b ) )
+( define (twolargestss a b c) ( sum-of-squares (returnlargest a b) (returnlargest a c)))
